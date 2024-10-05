46b0debf841d80e2ff5262bb369f39cdd756dcb3
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-10-05 17:38:08 +0300

    Adding the Attendance RSVP Feature

--- app/[id].tsx
+++ app/[id].tsx
@@ -90,7 +90,7 @@ export default function EventPage() {
         <Text className="text-xl font-semibold">Free</Text>
 
         {attendace ? (
-          <Text className='font-bold'>You are attending</Text>
+          <Text className='font-bold text-green-500'>You are attending</Text>
         ) : (
           <Pressable onPress={() => joinEvent()} className="rounded-md bg-red-400 p-5 px-8">
             <Text className="text-lg font-bold text-white">Join and RSVP</Text>
