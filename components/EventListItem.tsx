0266ac9ef26e91e56941d89838cfa2897fa93e07
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-10-05 18:09:59 +0300

    Displaying the Event Attendance

--- components/EventListItem.tsx
+++ components/EventListItem.tsx
@@ -2,11 +2,15 @@ import Feather from '@expo/vector-icons/Feather';
 import dayjs from 'dayjs';
 import { Link } from 'expo-router';
 import { View, Text, Image, Pressable } from 'react-native';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // @ts-ignore
 export default function EventListItem({ event }) {
   const [numberOfAttendees, setNumberOfAttendees] = useState(0);
+
+  useEffect(() => {
+    fetchNumberOfAttendees();
+  }, [event.id]);
 
   const fetchNumberOfAttendees = async () => {
 
