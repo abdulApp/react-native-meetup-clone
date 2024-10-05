25cb37664b80e7973efe35f74f9d247260a48131
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-10-05 15:22:09 +0300

    ignore error

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,9 +1,9 @@
 import dayjs from 'dayjs';
 import { useLocalSearchParams, Stack } from 'expo-router';
+import { useEffect, useState } from 'react';
 import { ActivityIndicator, Image, Pressable, Text, View } from 'react-native';
 
 import { supabase } from '~/utils/supabase';
-import { useEffect, useState } from 'react';
 
 export default function EventPage() {
   const { id } = useLocalSearchParams();
@@ -16,13 +16,13 @@ export default function EventPage() {
   }, [id]);
 
   const fetchEvent = async () => {
-    setLoading(true)
+    setLoading(true);
     const { data, error } = await supabase.from('events').select('*').eq('id', id).single();
     setEvent(data);
-    setLoading(false)
+    setLoading(false);
   };
 
-  if(loading) {
+  if (loading) {
     return <ActivityIndicator />;
   }
 
@@ -32,14 +32,37 @@ export default function EventPage() {
   return (
     <View className="flex-1 bg-white p-3">
       <Stack.Screen
+        //@ts-ignore
         options={{ title: event.title, headerBackTitleVisible: false, headerTintColor: 'red' }}
       />
-      <Image source={{ uri: event.image_url }} className="aspect-video w-full rounded-xl" />
-      <Text className="text-xl font-bold">{event.title}</Text>
-      <Text className="text-lg font-semibold uppercase text-amber-800">
-        {dayjs(event.date).format('ddd, D MMM')} · {dayjs(event.datetime).format('h:mm A')}
+      <Image
+        //@ts-ignore
+        source={{ uri: event.image_url }}
+        className="aspect-video w-full rounded-xl"
+      />
+      <Text className="text-xl font-bold">
+        {
+          //@ts-ignore
+          event.title
+        }
       </Text>
-      <Text className="text-lg font-bold">{event.description}</Text>
+      <Text className="text-lg font-semibold uppercase text-amber-800">
+        {
+          //@ts-ignore
+          dayjs(event.date).format('ddd, D MMM')
+        }{' '}
+        ·
+        {
+          //@ts-ignore
+          dayjs(event.date).format('h:mm A')
+        }
+      </Text>
+      <Text className="text-lg font-bold">
+        {
+          //@ts-ignore
+          event.description
+        }
+      </Text>
 
       {/*  Footer */}
       <View className="absolute bottom-0 left-0 right-0 flex-row items-center justify-between border-t-2 border-gray-400 p-5 pb-10">
