ea1251b0cb4a04b99823fbb21d98e48635129fa2
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-10-05 19:58:22 +0300

    Displaying the Event Attendance

--- app/event/[id]/attendance.tsx
+++ app/event/[id]/attendance.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Text } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
 
 export default function EventAttendance() {
+  const { id } = useLocalSearchParams();
   return (
     <Text>Hello</Text>
   )
