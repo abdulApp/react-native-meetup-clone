0e9e5f7352825d874c0bf7b440f2f919bcd31f85
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-10-05 19:59:59 +0300

    Displaying the Event Attendance

--- app/event/[id]/attendance.tsx
+++ app/event/[id]/attendance.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
+import { supabase } from '~/utils/supabase';
 
 export default function EventAttendance() {
   const { id } = useLocalSearchParams();
+
+  const [attendees, setAttendees] = useState(0);
+
+  useEffect(() => {
+    fetchNumberOfAttendees();
+  }, [id]);
+
+  const fetchNumberOfAttendees = async () => {
+    const { count } = await supabase
+      .from('attendance')
+      .select('*', { count: 'exact', head: true })
+      .eq('event_id', id);
+
+    // @ts-ignore
+    setAttendees(count);
+  }
   return (
     <Text>Hello</Text>
   )
