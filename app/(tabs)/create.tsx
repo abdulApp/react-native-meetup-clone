7201003b9cf30608ff2e6c759dff486027416501
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-10-08 00:23:24 +0300

    Adding the Create Event Feature

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -2,7 +2,7 @@ import { Text, TextInput, View } from 'react-native';
 
 export default function CreateEvent() {
   return (
-    <View className="flex-1 bg-white p-5 gap-2">
+    <View className="flex-1 gap-2 bg-white p-5">
       <Text>Create Event</Text>
       <TextInput placeholder="Title" className="rounded-md bg-gray-200 p-3 px-8" />
       <TextInput placeholder="Description" className="rounded-md bg-gray-200 p-3 px-8" />
