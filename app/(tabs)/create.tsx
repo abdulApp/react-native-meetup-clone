a6b5a8c934080da76330d7cfe7647d49b4cf6265
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-10-08 00:22:49 +0300

    Adding the Create Event Feature

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -4,7 +4,7 @@ export default function CreateEvent() {
   return (
     <View className="flex-1 bg-white p-5">
       <Text>Create Event</Text>
-      <TextInput placeholder="title" className="rounded-md bg-gray-200 p-3 px-8" />
+      <TextInput placeholder="Title" className="rounded-md bg-gray-200 p-3 px-8" />
       <TextInput placeholder="Description" className="rounded-md bg-gray-200 p-3 px-8" />
     </View>
   );
