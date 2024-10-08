c7739f7eb5527188301d64f0150bb95627cdc44a
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-10-08 18:18:32 +0300

    Adding the Create Event Feature

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,6 +1,8 @@
 import { Text, TextInput, View } from 'react-native';
+import DatePicker from 'react-native-date-picker';
 
 export default function CreateEvent() {
+
   return (
     <View className="flex-1 gap-3 bg-white p-5">
       <Text>Create Event</Text>
@@ -13,6 +15,8 @@ export default function CreateEvent() {
         numberOfLines={3}
         className="min-h-32 rounded-md bg-gray-200 p-3 px-8"
       />
+
+      <DatePicker date={} />
     </View>
   );
 }
