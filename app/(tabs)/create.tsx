a290076fbe26f0abb5eaa7209a74828b33d35247
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-10-12 00:24:56 +0300

    Adding the Create Event Feature

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -7,7 +7,7 @@ import { Button, Pressable, Text, TextInput, View } from 'react-native';
 export default function CreateEvent() {
   const [date, setDate] = useState(new Date());
 
-  const [title, setTitle] = useState("");
+  const [title, setTitle] = useState('');
   const [loading, setLoading] = useState(false);
   // const [open, setOpen] = useState(false);
 
