a11129d1ce84e6522681fcb63a8ff46f0b2dd09e
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-10-11 23:58:45 +0300

    Adding the Create Event Feature

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -55,8 +55,8 @@ export default function CreateEvent() {
       <Pressable
         onPress={() => createEvent()}
         disabled={loading}
-        className="mt-auto items-center rounded-md bg-blue-500 p-3 px-8">
-        <Text className="text-lg font-bold text-black">Create Event</Text>
+        className="mt-auto items-center rounded-md bg-red-500 p-3 px-8">
+        <Text className="text-lg font-bold text-white">Create Event</Text>
       </Pressable>
     </View>
   );
